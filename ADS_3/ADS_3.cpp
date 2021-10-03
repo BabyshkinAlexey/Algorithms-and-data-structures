c74e1f4434de39774ddd5124ed5db6764d24a67e
Бабушкин Алексей Сергеевич <77438776+BabyshkinAlexey@users.noreply.github.com>
2021-10-03 20:22:33 +0500

    Add files via upload

---
+++ ADS_3/ADS_3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream> 
+#include "locale.h"
+using namespace std;
+
+
+bool test(const int a) 
+{
+    if (a > 1)
+    {
+        for (int i = 2; i < a; i++)
+            if (a % i == 0)
+                return false;
+        return true;
+    }
+    else
+        return false;
+}
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите чило для проверки на простоту" << endl;
+    int a;
+    cin >> a;
+    cout << (test(a) ? "Число простое" : "Число не простое") << endl;
+    return 0;
+}
